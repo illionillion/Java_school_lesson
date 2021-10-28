ab48dd6a9cbcb116d6b67bd71ecc1d948e392774
illionillion <yuto.0526.sano@gmail.com>
2021-10-28 22:08:53 +0900

    九九表完成、2次元配列に格納して、for文で一個ずつ呼び出し

--- kuku_table.java
+++ kuku_table.java
@@ -1,22 +1,29 @@
-import java.lang.reflect.Array;
-
 /**
  * kuku_table
  */
 public class kuku_table {
 
     public static void main(String[] args) {
-        int row=1;
-        int col=1;
-        int kuku_arr[][] =new int[9][9];
-        for (int i =0 ; i < 10; i++) {
 
-            for (int j = 0; j < 10; j++) {
-                kuku_arr[i][j]=row*col;
-                col++;
+        int kuku_arr[][] =new int[9][9];//九九表を格納する２次元配列
+        for (int i =0 ; i < 9; i++) {
+
+            for (int j = 0; j < 9; j++) {
+                kuku_arr[i][j]=(i+1)*(j+1);//0から始まってるので+1する
             }
-            row++;
         }
-        // System.out.println(Arrays.toString(kuku_arr));
+        for (int i =0 ; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (kuku_arr[i][j]<10) {//一桁だった場合は「0」を前に付けて出力
+                    System.out.print("0"+kuku_arr[i][j]);
+                }else{
+                    System.out.print(kuku_arr[i][j]);
+                }
+                if (j<8) {//jが8より小さい場合は「,」で区切る
+                    System.out.print(",");
+                }
+            }
+            System.out.println();
+        }
     }
 }
