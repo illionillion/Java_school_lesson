02e03cd23e4996bfe44a7e4f73febfd5f4e01b53
illionillion <yuto.0526.sano@gmail.com>
2021-10-28 22:09:31 +0900

    String.equalsで文字列を比較

---
+++ Stringequal.java
@@ -0,0 +1,15 @@
+/**
+ * Stringequal
+ */
+public class Stringequal {
+
+    public static void main(String[] args) {
+        String a="nameA";
+        String b="nameB";
+        String ac="nameA";
+
+
+        System.out.println(a.equals(b));
+        System.out.println(a.equals(ac));
+    }
+}
